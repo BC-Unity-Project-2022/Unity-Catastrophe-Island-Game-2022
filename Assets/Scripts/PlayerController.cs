597ee0069802af53dfe625e442c7f0c880306565
micnekr <misha.nekrasov@gmail.com>
2022-01-11 12:24:21 +0000

    Some networking improvements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 struct UserInput{
@@ -8,18 +10,27 @@ struct UserInput{
 }
 public class PlayerController : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
-    public NetworkVariable<Quaternion> rotation = new NetworkVariable<Quaternion>();
+    // public NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
+    // public NetworkVariable<Quaternion> rotation = new NetworkVariable<Quaternion>();
+
+    private UserInput s_userInput;
 
     public const float WalkingSpeed = 1;
 
     private UserInput _userInput;
 
+    private void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        
+        rb.AddForce(new Vector3(s_userInput.DesiredDirection.x, 0, s_userInput.DesiredDirection.z));
+    }
+
     void SyncUserInput()
     {
         if (NetworkManager.Singleton.IsServer)
         {
-            SeverApplyUserInput(_userInput);
+            ServerApplyUserInput(_userInput);
         }
         else
         {
@@ -30,20 +41,23 @@ public class PlayerController : NetworkBehaviour
     [ServerRpc]
     void RequestInputUpdateServerRpc(UserInput userInput)
     {
-        SeverApplyUserInput(userInput);
+        ServerApplyUserInput(userInput);
     }
 
-    void SeverApplyUserInput(UserInput userInput)
+    void ServerApplyUserInput(UserInput userInput)
     {
-        position.Value += new Vector3(userInput.DesiredDirection.x, 0, userInput.DesiredDirection.z);
+
+        s_userInput = userInput;
+
+        // position.Value += new Vector3(userInput.DesiredDirection.x, 0, userInput.DesiredDirection.z);
     }
 
     void Update()
     {
         // https://github.com/JetBrains/resharper-unity/wiki/Avoid-multiple-unnecessary-property-accesses
         var transform1 = transform;
-        transform1.position = position.Value;
-        transform1.rotation = rotation.Value;
+        // transform1.position = position.Value;
+        // transform1.rotation = rotation.Value;
 
         // incorporate the raw values of horizontal movement
         _userInput.DesiredDirection = transform1.forward * Input.GetAxis("Horizontal") +
