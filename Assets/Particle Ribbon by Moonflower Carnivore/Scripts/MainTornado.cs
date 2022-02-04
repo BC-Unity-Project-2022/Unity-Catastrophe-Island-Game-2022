0863024e4484f692f916ab4011aab1cd662dc6f6
08csmith <08csmith@brightoncollege.net>
2022-02-04 17:14:23 +0000

    pulled

--- Assets/Particle Ribbon by Moonflower Carnivore/Scripts/MainTornado.cs
+++ Assets/Particle Ribbon by Moonflower Carnivore/Scripts/MainTornado.cs
@@ -35,4 +35,53 @@ public class MainTornado : MonoBehaviour
 
         }
     }
+    // Range over which perlin varies.
+    float perlinScale = 1.0f;
+
+    // Distance covered per second along X axis of Perlin plane.
+    public float xScale = 1.0f;
+
+    public float speed = 2;
+
+    private float halfMap = 125;
+
+    public ParticleSystem ps;
+
+    public bool disabled = false;
+
+    void Start()
+    {
+        var main = ps.main;
+        main.loop = true;
+    }
+
+    void Update()
+    {
+        float perlin = perlinScale * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
+
+        //Y coordinate
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(transform.position);
+        transform.position = pos;
+
+        //Facing direction
+        Vector3 direction = Vector3.forward;
+        direction = Quaternion.Euler(0, perlin * 360, 0) * direction;
+
+        //Move forward
+        transform.position += (direction * speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, pos.y, transform.position.z);
+
+        if (transform.position.x > halfMap | transform.position.x < -halfMap)
+        {
+            ps.Stop(true);
+            disabled = true;
+        }
+        if (transform.position.z > halfMap | transform.position.z < -halfMap)
+        {
+            ps.Stop(true);
+            disabled = true;
+        }
+    }
 }
--- Assets/Scripts/Disasters/PerlinMove.cs
+++ Assets/Scripts/Disasters/PerlinMove.cs
@@ -4,53 +4,50 @@ using UnityEngine;
 
 public class PerlinMove : MonoBehaviour
 {
-    // "Bobbing" animation from 1D Perlin noise.
-
-    // Range over which height varies.
-    float heightScale = 1.0f;
+    
+    // Range over which perlin varies.
+    float perlinScale = 1.0f;
 
     // Distance covered per second along X axis of Perlin plane.
     public float xScale = 1.0f;
 
     public float speed = 2;
 
-    public float degreesPerSecond = 20;
-
     private float halfMap = 125;
+
+    public ParticleSystem ps;
+
+    void Start()
+    {
+        var main = ps.main;
+        main.loop = true;
+    }
 
     void Update()
     {
-        float height = heightScale * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
-        //Vector3 pos = transform.position;
-        //pos.y = height;
-        //transform.position = pos;
+        float perlin = perlinScale * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
 
+        //Y coordinate
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(transform.position);
+        transform.position = pos;
+
+        //Facing direction
         Vector3 direction = Vector3.forward;
+        direction = Quaternion.Euler(0, perlin * 360, 0) * direction;
 
-        direction = Quaternion.Euler(0, height * 360, 0) * direction;
-
-        //Debug.Log(height * 360);
-        //transform.Rotate(new Vector3(0, height, 0));
-
-        //transform.Translate(0, 0, 1);
-
-        transform.position += direction * speed * Time.deltaTime;
-
-        
-        //Debug.Log("X = ");
-        //Debug.Log(transform.position.x);
-        //Debug.Log("Y = ");
-        //Debug.Log(transform.position.y);
-        //Debug.Log("Z = ");
-        //Debug.Log(transform.position.z);
+        //Move forward
+        transform.position += (direction * speed * Time.deltaTime);
+       
+        transform.position = new Vector3(transform.position.x, pos.y, transform.position.z);
 
         if (transform.position.x > halfMap | transform.position.x < -halfMap)
         {
-            Debug.Log("5");
+            ps.Stop(true);
         }
         if (transform.position.z > halfMap | transform.position.z < -halfMap)
         {
-            Debug.Log("5");
+            ps.Stop(true);
         }
     }
 }
